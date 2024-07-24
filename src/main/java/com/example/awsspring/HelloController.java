a57a92cb66fa0eec9fde93ab713f30cb1d951f2d
yeon <p-yo00@naver.com>
2024-07-24 10:33:54 +0900

    initial commit

---
+++ src/main/java/com/example/awsspring/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.awsspring;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class HelloController {
+
+    private final PostRepository postRepository;
+
+    @PostMapping("/post")
+    public void savePost() {
+        postRepository.save(Post.builder()
+            .title("제목")
+            .content("내용")
+            .build());
+    }
+
+    @GetMapping("/post")
+    public ResponseEntity<Post> getPost() {
+        return ResponseEntity.ok(postRepository.findAll().get(0));
+    }
+
+    @GetMapping
+    public String test() {
+        return "Hello!!!";
+    }
+}
